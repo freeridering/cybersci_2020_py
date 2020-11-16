bbe1ff1eafb5d98bbb1d5f4b3a2a5057ce18d817
freeridering <1102509625@qq.com>
2020-11-16 16:57:43 +0800

    Initial commit

---
+++ scheduling.py
@@ -0,0 +1,26 @@
+class event_scheduling:
+    def __init__(self):
+        event_list = []
+        timing = 0
+
+    def add_evevt(self):
+        """
+        添加事件
+        :return:
+        """
+        pass
+
+    def fetch_event(self):
+        """
+        取符合要求的事件
+        :return:
+        """
+        pass
+
+    def scheduling(self):
+        """
+        调度过程主体
+        :return:
+        """
+        pass
+
